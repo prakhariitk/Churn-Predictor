e72a9b5397e07eacda9fa3792244a0696053fe39
Prakhar Pradhan <65254289+prakhariitk@users.noreply.github.com>
2021-07-11 22:34:10 +0530

    Update Model.py
    model selection among random forest and Decision Tree Classifier, Feature engineering, handling imbalanced data using upsampling and PCA and

--- Model.py
+++ Model.py
@@ -2,9 +2,9 @@
 """
 Created on Mon Jul  5 15:22:58 2021
 
-@author: LENOVO
+@author: Pradhan
 """
-
+#importing necesary libraries
 import pandas as pd
 import numpy as np
 import seaborn as sns
@@ -21,7 +21,7 @@ from imblearn.combine import SMOTEENN
 
 churn_data = pd.read_csv('Telco_Customer_Churn_Data.csv')
 churn_data
-
+# Exploring Data and feature engineering.
 churn_data.head()
 churn_data.TotalCharges = pd.to_numeric(churn_data.TotalCharges, errors='coerce')
 churn_data.dropna(how = 'any', inplace=True)
@@ -32,9 +32,9 @@ churn_data['Churn']= np.where(churn_data.Churn=='Yes',1,0)
 churn_data_converted = pd.get_dummies(churn_data)
 churn_data_converted.head()
 churn_data_converted.shape
-
+#rsetting index
 churn_data_converted= churn_data_converted.reset_index(drop=True)
-
+#splitting the data
 X = churn_data_converted.drop('Churn',axis=1)
 X
 
@@ -42,7 +42,7 @@ Y = churn_data_converted['Churn']
 Y
 
 x_train,x_test,y_train,y_test=train_test_split(X,Y,test_size=0.2)
-
+#fitting Decision Tree Classifier
 model_dt=DecisionTreeClassifier(criterion = "gini",random_state = 100,max_depth=6, min_samples_leaf=8)
 model_dt.fit(x_train,y_train)
 y_pred=model_dt.predict(x_test)
@@ -53,7 +53,7 @@ model_dt.score(x_test,y_test)
 
 print(classification_report(y_test, y_pred, labels=[0,1]))
 
-
+# handling imbalanced data using upsampling.
 sm = SMOTEENN()
 X_resampled, y_resampled = sm.fit_resample(X,Y)
 xr_train,xr_test,yr_train,yr_test=train_test_split(X_resampled, y_resampled,test_size=0.2)
@@ -65,7 +65,7 @@ print(model_score_r)
 print(metrics.classification_report(yr_test, yr_predict))
 
 print(metrics.confusion_matrix(yr_test, yr_predict))
-
+# fitting randomforestclassifier
 from sklearn.ensemble import RandomForestClassifier
 model_rf=RandomForestClassifier(n_estimators=100, criterion='gini', random_state = 100,max_depth=6, min_samples_leaf=8)
 model_rf.fit(x_train,y_train)
@@ -87,7 +87,7 @@ print(metrics.classification_report(yr_test1, yr_predict1))
 
 
 print(metrics.confusion_matrix(yr_test1, yr_predict1))
-
+# handling data using PCA 
 from sklearn.decomposition import PCA
 pca = PCA(0.9)
 xr_train_pca = pca.fit_transform(xr_train1)
